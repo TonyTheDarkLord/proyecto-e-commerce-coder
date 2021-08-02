9400761e9301ef6bbb8127d5c5fe7cb5be3b175e
Juan Pablo Toniolo <jtoniolo@udesa.edu.ar>
2021-08-01 23:14:20 -0300

    cambios primer clase ReactJS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import 'animate.css'
+import logoCoder from './coderhouse-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://plataforma.coderhouse.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src={logoCoder} className="animate__animated App-logo animate__repeat-3" alt="logo" />
         </a>
+        <h3>Camada 19720</h3>
+        <h4>Juan Pablo Toniolo</h4>
       </header>
     </div>
   );

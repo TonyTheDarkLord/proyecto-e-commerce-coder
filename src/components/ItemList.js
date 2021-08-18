b4aa6dd2f7396a7e3635668221e55c2dc3ecf306
Juan Pablo Toniolo <jtoniolo@udesa.edu.ar>
2021-08-17 23:54:41 -0300

    Agregue item, promise y UseEffect

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,26 +1,39 @@
-import React from 'react'
-import ItemListContainer from './ItemListContainer'
+import React, {useState, useEffect} from 'react'
+import Item from './Item'
 
-//JSON DATA
-import DataProds from '../assets/productos.json'
+//JSON
+import Prods from '../assets/productos.json'
 
 const ItemList = () => {
+
+    const [DataProds, setProductos] = useState([]);
+
+    useEffect(() =>{
+        new Promise((resolve, reject) => {
+            setTimeout(() =>{
+                setProductos(Prods)
+                resolve(true);
+            },2000)
+        })
+    })
+    
     return (
         <div className="row">
-            {DataProds.map((producto) =>{
-                return(
-                    <ItemListContainer
-                        key={producto.id}
-                        title={producto.title}
-                        img={producto.img}
-                        description={producto.description}
-                        price={producto.price}
-                        stock={producto.stock}
-                    />
-                );
-            })}
+                {DataProds.map((producto) =>{
+                    return(
+                        <Item
+                            key={producto.id}
+                            title={producto.title}
+                            img={producto.img}
+                            description={producto.description}
+                            price={producto.price}
+                            stock={producto.stock}
+                        />
+                    );
+                })}
         </div>
     )
+
 }
 
 export default ItemList
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,23 +1,11 @@
 import React from 'react'
-import ItemCount from './ItemCount'
+import ItemList from './ItemList'
 
-const itemListContainer = ({title,description,price,stock,img}) =>{
+
+const itemListContainer = () =>{
     return(
         <>
-            <div className="col s12 m6 l4">
-            <div className="card hoverable">
-                <div className="card-image valign-wrapper">
-                <img src={img} alt=""/>
-                <a className="btn-floating halfway-fab waves-effect waves-light red" href="/#c"><i className="material-icons">add_shopping_cart</i></a>
-                </div>
-                <div className="card-content">
-                <span className="card-title">{title}</span>
-                <p className="card-desc">{description}</p>
-                <h4>${price}</h4>
-                <span>Cantidad : <ItemCount stock={stock}/></span>
-                </div>
-            </div>
-            </div>
+            <ItemList></ItemList>
         </>
     )
 }
